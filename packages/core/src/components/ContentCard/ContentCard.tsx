eaeccee83d6aacd69bd5b077a93cb2edb01c7347
RamazanIttiev <ramazzan.ittiev@gmail.com>
2026-08-18 17:02:47 +0100

    Update ContentCard styles and improve localization strings for clarity
    Signed-off-by: RamazanIttiev <ramazzan.ittiev@gmail.com>

--- packages/core/src/components/ContentCard/ContentCard.tsx
+++ packages/core/src/components/ContentCard/ContentCard.tsx
@@ -50,7 +50,7 @@ export function ContentCard(props: ContentCardProps) {
   return (
     <Card
       variant='secondary'
-      className={`flex flex-col border-gray-500 border border-solid items-center gap-3 p-6 text-center shadow-surface transition-all duration-300 hover:scale-[1.03] hover:shadow-md cursor-default ${className ?? ''}`}
+      className={`flex flex-col h-full border-gray-500 border border-solid items-center gap-3 p-6 text-center shadow-surface transition-all duration-300 hover:scale-[1.03] hover:shadow-md cursor-default ${className ?? ''}`}
     >
       <div className='text-primary'>{icon}</div>
       <Card.Header className='flex-col items-center gap-1 p-0'>
