50d768d5dd47514f0119b56235ec108e20584bce
RamazanIttiev <ramazzan.ittiev@gmail.com>
2026-09-21 16:41:50 +0100

    feat: update migration to drop 'source' column conditionally and maintain userId nullability
    Signed-off-by: RamazanIttiev <ramazzan.ittiev@gmail.com>

--- packages/database/src/migrations/1789995594785-Migration.ts
+++ packages/database/src/migrations/1789995594785-Migration.ts
@@ -1,18 +1,21 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Migration1789995594785 implements MigrationInterface {
-    name = 'Migration1789995594785'
+  name = 'Migration1789995594785';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "tolt_referral" DROP COLUMN "source"`);
-        await queryRunner.query(`ALTER TABLE "tolt_referral" DROP COLUMN "commissionEndedAt"`);
-        await queryRunner.query(`ALTER TABLE "yookassa_payments" ALTER COLUMN "userId" DROP NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "tolt_referral" DROP COLUMN IF EXISTS "source"`);
+    await queryRunner.query(`ALTER TABLE "tolt_referral" DROP COLUMN "commissionEndedAt"`);
+    await queryRunner.query(`ALTER TABLE "yookassa_payments" ALTER COLUMN "userId" DROP NOT NULL`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "yookassa_payments" ALTER COLUMN "userId" SET NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "tolt_referral" ADD "commissionEndedAt" TIMESTAMP WITH TIME ZONE`);
-        await queryRunner.query(`ALTER TABLE "tolt_referral" ADD "source" character varying NOT NULL DEFAULT 'affiliate'`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "yookassa_payments" ALTER COLUMN "userId" SET NOT NULL`);
+    await queryRunner.query(
+      `ALTER TABLE "tolt_referral" ADD "commissionEndedAt" TIMESTAMP WITH TIME ZONE`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "tolt_referral" ADD "source" character varying NOT NULL DEFAULT 'affiliate'`,
+    );
+  }
 }
